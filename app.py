13d97c567e6a370cd7f6f4ce16f91bdd437e5448
Saiteja <123763978+Saiteja-203@users.noreply.github.com>
2025-03-14 12:08:37 +0000

    Commit

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ with st.container():
         )
     
     with img:
-        st.image(cer2_img,width=400)
+        st.image(cer2_img,height=200)
     with text:
         st.subheader('PwC Switzerland Cybersecurity Job Simulation on Forage - February 2025')
         st.write(
